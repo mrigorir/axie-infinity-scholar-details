5a70d04de006ac2f9d35904d6d02e3c07523882e
Marco Parra <marcoparra311@gmail.com>
2021-11-29 16:55:30 -0400

    Use localStorage to preserve slp, mmr and axies data

--- src/hooks/useDatahooks.js
+++ src/hooks/useDatahooks.js
@@ -5,16 +5,31 @@ import { getMmrAction } from '../redux/mmr';
 import { getAxiesAction } from '../redux/axies';
 
 const useDatahooks = () => {
-  const slp = useSelector((state) => state.slp);
-  const mmr = useSelector((state) => state.mmr);
-  const axies = useSelector((state) => state.axies);
+  let slp = useSelector((state) => state.slp);
+  let mmr = useSelector((state) => state.mmr);
+  let axies = useSelector((state) => state.axies);
   const roninRef = useRef();
-
   const dispatch = useDispatch();
+  const LOCAL_STORAGE_SLP = 'slp';
+  const LOCAL_STORAGE_MMR = 'mmr';
+  const LOCAL_STORAGE_AXIES = 'axies';
+  const storedSLP = JSON.parse(localStorage.getItem(LOCAL_STORAGE_SLP));
+  const storedMMR = JSON.parse(localStorage.getItem(LOCAL_STORAGE_MMR));
+  const storedAxies = JSON.parse(localStorage.getItem(LOCAL_STORAGE_AXIES));
 
   useEffect(() => {
-    console.log('useEffect working when page loads.');
+    if (storedSLP && storedMMR && storedAxies) {
+      slp = storedSLP;
+      mmr = storedMMR;
+      axies = storedAxies;
+    }
   }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_SLP, JSON.stringify(slp));
+    localStorage.setItem(LOCAL_STORAGE_MMR, JSON.stringify(mmr));
+    localStorage.setItem(LOCAL_STORAGE_AXIES, JSON.stringify(axies));
+  }, [slp]);
 
   return {
     slp,
